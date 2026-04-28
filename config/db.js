e4602cb79c5a6d89dec648758455c5f2a3e33772
jmlangbubu <101802775+jmlangbubu@users.noreply.github.com>
2026-04-28 12:44:01 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,23 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection(process.env.DATABASE_URL);
+const db = mysql.createPool({
+  uri: process.env.DATABASE_URL,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
+});
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
-    console.error("❌ Database connection failed:", err);
+    console.error("❌ MySQL pool connection failed:", err);
     return;
   }
-  console.log("✅ MySQL connected successfully");
+
+  console.log("✅ MySQL pool connected successfully");
+  connection.release();
 });
 
 module.exports = db;
