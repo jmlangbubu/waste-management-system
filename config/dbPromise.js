6bc3d78e05f2a6f63c3d4353163a5d8561e47ba3
jmlangbubu <101802775+jmlangbubu@users.noreply.github.com>
2026-04-28 13:19:40 +0800

    Update dbPromise.js

--- config/dbPromise.js
+++ config/dbPromise.js
@@ -1,13 +1,14 @@
 const mysql = require("mysql2/promise");
+require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "ai_waste_management",
+  uri: process.env.DATABASE_URL,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  enableKeepAlive: true
 });
+
+console.log(" MySQL promise pool configured");
 
 module.exports = pool;
