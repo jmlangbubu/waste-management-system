e471bf705d57d7cc0f03968ee8fe63c388860d31
jmlangbubu <101802775+jmlangbubu@users.noreply.github.com>
2026-04-30 02:38:57 +0800

    Update appointmentRoutes.js

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -6,9 +6,9 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "wastemanagementgensan00@gmail.com",
-    pass: "lgqbzswacdxofrib"
-  }
+  user: process.env.EMAIL_USER,
+  pass: process.env.EMAIL_PASS
+}
 });
 
 console.log("appointmentRoutes loaded");
